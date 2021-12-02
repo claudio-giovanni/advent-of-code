a91443732d995efdc5c7f46e25f6fbbad67b2e12
Claudio <claudio.enoch@gmail.com>
2021-12-02 12:26:28 -0700

    fixed needless slicing

--- 2021/01.py
+++ 2021/01.py
@@ -3,14 +3,14 @@ numbers = [int(number) for number in numbers]
 
 
 def count_increases(data: list[int]) -> int:
-    return sum((data[i + 1] - data[i]) > 0 for i in range(len((data[:-1]))))
+    return sum((data[i + 1] - data[i]) > 0 for i in range(len(data) - 1))
 
 
 part_one = count_increases(numbers)
 print("PART ONE: ", part_one)
 
 three_window_numbers = []
-for i in range(len(numbers[:-2])):
+for i in range(len(numbers) - 2):
     three_window_numbers.append(sum((numbers[i], numbers[i + 1], numbers[i + 2])))
 
 part_two = count_increases(three_window_numbers)

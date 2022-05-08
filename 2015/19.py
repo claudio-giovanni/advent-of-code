5ac41b792960ede04dce3863fe3aeedf114dd144
Claudio <claudio.enoch@gmail.com>
2022-05-07 19:39:07 -0600

    15-19

--- 2015/19.py
+++ 2015/19.py
@@ -1,34 +1,35 @@
 import re
+import random
 
 data = open("19.txt").read().splitlines()
 conversion_data = data[:-2]
-conversion_data = tuple((c[0], c[1]) for conversion in conversion_data if (c := conversion.split(" => ")))
+conversion_data = list((c[0], c[1]) for conversion in conversion_data if (c := conversion.split(" => ")))
 
 molecule_data = data[-1]
 
 
 class FusionPlant:
-    def __init__(self, conversion: tuple[tuple[str, str]]):
+    def __init__(self, conversion: list[tuple[str, str]]):
         self.conversions = conversion
-        self.successful_fabrications = []
 
     def calibrate(self, molecule: str) -> set[str]:
         mutations: set[str] = set()
-        for partial, replacement in self.conversions:
-            for match in re.finditer(partial, molecule):
-                mutations.add(f"{molecule[:match.start()]}{replacement}{molecule[match.end():]}")
+        for reactant, product in self.conversions:
+            for match in re.finditer(reactant, molecule):
+                mutations.add(f"{molecule[:match.start()]}{product}{molecule[match.end():]}")
         return mutations
 
-    def fabricate(self, current_molecule: str, end_molecule: str, iteration: int = 1) -> int:
-        for mutation in self.calibrate(molecule=current_molecule):
-            if mutation == end_molecule:
-                self.successful_fabrications.append(iteration)
-            if len(mutation) > len(end_molecule):
-                continue
-            self.fabricate(current_molecule=mutation, end_molecule=end_molecule, iteration=iteration + 1)
-        return min(self.successful_fabrications, default=0)
+    # Forgive them for they know not what they do
+    def fabricate(self, start_molecule: str, end_molecule: str) -> int:
+        count = 0
+        while end_molecule != start_molecule:
+            reactant, product = random.choice(self.conversions)
+            if product in end_molecule:
+                end_molecule = end_molecule.replace(product, reactant, 1)
+                count += 1
+        return count
 
 
 fusion_plant = FusionPlant(conversion=conversion_data)
 print(f"PART ONE: {len(fusion_plant.calibrate(molecule_data))}")
-print(f"PART TWO: {fusion_plant.fabricate(current_molecule='e', end_molecule=molecule_data)}")
+print(f"PART TWO: {fusion_plant.fabricate(start_molecule='e', end_molecule=molecule_data)}")

7b5c2927bacd5f4753598f63c1182f54491d4771
Claudio <claudio.enoch@gmail.com>
2022-06-12 11:36:08 -0600

    2015 22

--- 2015/22.py
+++ 2015/22.py
@@ -2,35 +2,64 @@ from __future__ import annotations
 
 from copy import deepcopy
 from dataclasses import dataclass, field
-from itertools import count, product
+from typing import Callable, Iterable
 
 data = open("22.txt").read().splitlines()
+
+
+class SpellFailedException(Exception):
+    pass
+
+
+class CharacterDiedException(Exception):
+    pass
 
 
 @dataclass
 class Spell:
     name: str
     mana_cost: int
-    damage: int
-    armor: int
-    hp_regen: int
-    mana_regen: int
-    target_self: bool
-    duration: int
+    cast: Callable[[Character, Character], None] = field(repr=False)
+    duration: int = field(repr=False)
+
+    def __bool__(self):
+        return self.duration > 0
+
+    def __hash__(self):
+        return hash(self.name)
 
     def __eq__(self, other: Spell):
-        return self.name == other.name
+        return hash(self) == hash(other)
 
 
-magic_missile = Spell(
-    name="magic_missile", mana_cost=49, damage=4, armor=0, hp_regen=0, mana_regen=0, target_self=False, duration=0
-)
-drain = Spell(name="drain", mana_cost=73, damage=2, armor=0, hp_regen=2, mana_regen=0, target_self=False, duration=0)
-shield = Spell(name="shield", mana_cost=113, damage=0, armor=7, hp_regen=0, mana_regen=0, target_self=True, duration=6)
-poison = Spell(name="poison", mana_cost=173, damage=3, armor=0, hp_regen=0, mana_regen=0, target_self=False, duration=6)
-recharge = Spell(
-    name="recharge", mana_cost=229, damage=0, armor=0, hp_regen=0, mana_regen=101, target_self=True, duration=5
-)
+class Effect:
+    @staticmethod
+    def magic_missile(_: Character, other: Character):
+        other.hit_points -= 4
+
+    @staticmethod
+    def drain(self: Character, other: Character):
+        other.hit_points -= 2
+        self.hit_points += 2
+
+    @staticmethod
+    def shield(self: Character, _: Character):
+        self.armor = 7
+
+    @staticmethod
+    def poison(_: Character, other: Character):
+        other.hit_points -= 3
+
+    @staticmethod
+    def recharge(self: Character, _: Character):
+        self.mana += 101
+
+
+magic_missile = Spell(name="magic_missile", mana_cost=53, cast=Effect.magic_missile, duration=0)
+drain = Spell(name="drain", mana_cost=73, cast=Effect.drain, duration=0)
+shield = Spell(name="shield", mana_cost=113, cast=Effect.shield, duration=6)
+poison = Spell(name="poison", mana_cost=173, cast=Effect.poison, duration=6)
+recharge = Spell(name="recharge", mana_cost=229, cast=Effect.recharge, duration=5)
 
 
 @dataclass
@@ -39,34 +68,37 @@ class Character:
     damage: int
     armor: int
     mana: int
-    dot: list[Spell] = field(init=False, repr=False, default_factory=dict)
+    eot: list[Spell] = field(init=False, repr=False, default_factory=list)
 
-    def __bool__(self):
-        return self.hit_points > 0
+    @property
+    def is_dead(self):
+        return self.hit_points <= 0
 
     def attack_with_weapon(self, other: Character):
-        other.hit_points = other.hit_points - max(self.damage - other.armor, 1)
+        other.hit_points -= max(self.damage - other.armor, 1)
+        if other.is_dead:
+            raise CharacterDiedException
 
-    def attack_with_spell(self, other: Character, spell: Spell):
-        if spell.name == drain.name:
-            self._resolve_spell(spell=spell)
-        if spell.target_self:
-            if spell.duration:
-                self.dot.append(deepcopy(spell))
-            else:
-                self._resolve_spell(spell=spell)
-        if spell.target_self is False:
-            if spell.duration:
-                other.dot.append(deepcopy(spell))
-            else:
-                other._resolve_spell(spell=spell)
-
-    def _resolve_spell(self, spell: Spell):
+    def cast_spell(self, other: Character, spell: Spell):
+        if spell.mana_cost > self.mana:
+            raise SpellFailedException("INSUFFICIENT MANA")
+        if spell in self.eot:
+            raise SpellFailedException("SPELL ALREADY IN EOT")
         self.mana -= spell.mana_cost
-        self.hit_points -= spell.damage
-        self.armor += spell.armor
-        self.hit_points += spell.hp_regen
-        self.mana += spell.mana_regen
+        self.eot.append(deepcopy(spell)) if spell.duration else spell.cast(self, other)
+        if self.is_dead or other.is_dead:
+            raise CharacterDiedException
+
+    def resolve_eot(self, other: Character):
+        for spell in self.eot:
+            spell.cast(self, other)
+            spell.duration -= 1
+            if spell == shield and spell.duration == 0:
+                self.armor = 0
+        if self.is_dead or other.is_dead:
+            raise CharacterDiedException
+
+        self.eot = list(filter(None, self.eot))
 
     @staticmethod
     def from_file(character_data: list[str]) -> Character:
@@ -74,40 +106,39 @@ class Character:
         return Character(hit_points=hit_points, damage=damage, armor=0, mana=0)
 
 
-spell_chain: list[list[Spell]] = [[magic_missile], [drain], [shield], [poison], [recharge]]
-
-
-def get_spell_permutations_by_mana(mana: int, spells_cast: list[list[Spell]] = None):
-    spells_cast = spells_cast or []
-    permutations = (cast + flat for cast, flat in product(spells_cast, spell_chain)) if spells_cast else spell_chain
-    for spells in permutations:
-        spells_mana_cost = sum(spell.mana_cost for spell in spells)
-        if spells_mana_cost < mana:
-            spells_cast.append(spells)
-            get_spell_permutations_by_mana(mana=mana, spells_cast=spells_cast)
-    return permutations
-
-
-print(get_spell_permutations_by_mana(100))
+def get_spell_permutations() -> Iterable[tuple[Spell], int]:
+    spell_mutations: tuple[tuple[Spell], ...] = ((magic_missile,), (drain,), (shield,), (poison,), (recharge,))
+    spells_cast = ((),)
+    while True:
+        valid_permutation = set()
+        for spells in spells_cast:
+            for new_spell in spell_mutations:
+                spell_permutation = spells + new_spell
+                permutation_cost = sum(spell.mana_cost for spell in spell_permutation)
+                yield spell_permutation, permutation_cost
+                valid_permutation.add(spell_permutation)
+        spells_cast = tuple(valid_permutation)
 
 
 def efficiency_battle_magic_beats_melee(wizard: Character, warrior: Character) -> int:
-    # successful_mana_spent = []
-    # while True:
-    #     for mana in count(1, 1):
-    #         for spell in spell_chain:
-    #             wizard.attack_with_spell(other=warrior, spell=spell)
-    #             if not warrior:
-    #                 successful_mana_spent =
-
-    pass
+    for spells, spells_cost in get_spell_permutations():
+        temp_wizard, temp_warrior = deepcopy(wizard), deepcopy(warrior)
+        for spell in spells:
+            try:
+                temp_wizard.resolve_eot(other=temp_warrior)
+                temp_wizard.cast_spell(other=temp_warrior, spell=spell)
+                temp_wizard.resolve_eot(other=temp_warrior)
+                temp_warrior.attack_with_weapon(temp_wizard)
+            except SpellFailedException:
+                break
+            except CharacterDiedException:
+                if temp_warrior.is_dead and spells_cost < 1280:
+                    return spells_cost
+                break
 
 
 adventurer = Character(hit_points=50, damage=0, armor=0, mana=500)
 boss = Character.from_file(data)
 
-# print(adventurer)
-# print(boss)
-efficiency_battle_magic_beats_melee(wizard=adventurer, warrior=boss)
-# print(f"PART ONE: {None}")
-# print(f"PART TWO: {None}")
+print(f"PART ONE: {efficiency_battle_magic_beats_melee(wizard=adventurer, warrior=boss)}")
+print(f"PART TWO: {None}")

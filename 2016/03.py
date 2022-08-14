1633352bafe35b289b3b92350e0398c58e985d7e
Claudio <claudio.enoch@gmail.com>
2022-08-13 19:58:07 -0600

    2016 03

--- 2016/03.py
+++ 2016/03.py
@@ -1,5 +1,40 @@
+from __future__ import annotations
+
+from itertools import chain
+
 data = open("03.txt").read().splitlines()
 
 
-print(f"PART ONE: {None}")
-print(f"PART TWO: {None}")
+class Triangle:
+    def __init__(self, sides: tuple[int, ...]):
+        self.sides = sides
+
+    @property
+    def is_valid(self):
+        return (max_side := max(self.sides)) < sum(self.sides) - max_side
+
+
+def get_triangles_from_data_horizontally(triangle_data: list[str]) -> list[Triangle]:
+    valid_triangles = []
+    for datum in triangle_data:
+        sides = tuple(int(number) for number in datum.strip().split())
+        if (triangle := Triangle(sides=sides)).is_valid:
+            valid_triangles.append(triangle)
+    return valid_triangles
+
+
+def get_triangles_from_data_vertically(triangle_data: list[str]) -> list[Triangle]:
+    valid_triangles = []
+    columns = list(zip(*[tuple(int(number) for number in datum.strip().split()) for datum in triangle_data]))
+    side_chain = list(chain(*columns))
+    for i in range(0, len(side_chain), 3):
+        if (triangle := Triangle(sides=tuple(side_chain[i : i + 3]))).is_valid:
+            valid_triangles.append(triangle)
+    return valid_triangles
+
+
+horizontal_triangles = get_triangles_from_data_horizontally(triangle_data=data)
+print(f"PART ONE: {len(horizontal_triangles)}")
+
+vertical_triangles = get_triangles_from_data_vertically(triangle_data=data)
+print(f"PART TWO: {len(vertical_triangles)}")

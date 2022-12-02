c68271f7a46a5d652c2fe6edcfa59514886cda64
Claudio <claudio.enoch@gmail.com>
2022-12-01 19:52:42 -0700

    2022 01

--- 2022/01.py
+++ 2022/01.py
@@ -1,5 +1,17 @@
+from dataclasses import dataclass
+
 data = open("01.txt").read().splitlines()
+data = [[int(i) for i in datum.split("\n") if i] for datum in data]
 
 
-print(f"PART ONE: {None}")
-print(f"PART TWO: {None}")
+@dataclass
+class Elf:
+    inventory: list[int]
+
+
+elves = [Elf(datum) for datum in data]
+
+sorted_elves = list(sorted((sum(elf.inventory) for elf in elves), reverse=True))
+
+print(f"PART ONE: {sorted_elves[0]}")
+print(f"PART TWO: {sum(sorted_elves[:3])}")

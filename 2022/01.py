994993be1ca2b1bf85e6243a5ca6ee55081a346f
Claudio <claudio.enoch@gmail.com>
2022-12-03 19:18:35 -0700

    improve performance

--- 2022/01.py
+++ 2022/01.py
@@ -1,17 +1,23 @@
+import re
 from dataclasses import dataclass
+from typing import Iterable
 
-data = open("01.txt").read().splitlines()
-data = [[int(i) for i in datum.split("\n") if i] for datum in data]
+data = open("01.txt").read()
+data = [map(int, i.split()) for i in re.findall(r"((?:\d+\n)+)", data)]
 
 
 @dataclass
 class Elf:
-    inventory: list[int]
+    inventory: Iterable[int]
+
+    @property
+    def calories(self) -> int:
+        return sum(self.inventory)
 
 
 elves = [Elf(datum) for datum in data]
 
-sorted_elves = list(sorted((sum(elf.inventory) for elf in elves), reverse=True))
+sorted_elves = list(sorted((elf.calories for elf in elves), reverse=True))
 
 print(f"PART ONE: {sorted_elves[0]}")
 print(f"PART TWO: {sum(sorted_elves[:3])}")

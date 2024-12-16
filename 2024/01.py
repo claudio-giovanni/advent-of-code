ce20a902efc24ad05225c0afb8f51975dad9d784
Claudio <claudio.enoch@gmail.com>
2024-12-15 21:30:57 -0700

    24 01

--- 2024/01.py
+++ 2024/01.py
@@ -1,5 +1,17 @@
+from collections import Counter
+
 data = open("01.txt").read().splitlines()
 
+data = [list(map(int, datum.split())) for datum in data]
 
-print(f"PART ONE: {None}")
-print(f"PART TWO: {None}")
+
+list_one, list_two = map(list, zip(*data))  # noqa
+list_one.sort(), list_two.sort()
+list_diff = sum(abs(item_one - item_two) for item_one, item_two in zip(list_one, list_two))
+
+list_two_counter = Counter(list_two)
+similarity_score = sum(list_two_counter.get(item_one, 0) * item_one for item_one in list_one)
+
+
+print(f"PART ONE: {list_diff}")
+print(f"PART TWO: {similarity_score}")

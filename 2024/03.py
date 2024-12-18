7e9f22b0a6251d3af8afeef4bba8d82bfc6559f9
Claudio <claudio.enoch@gmail.com>
2024-12-17 20:59:47 -0700

    24 03

--- 2024/03.py
+++ 2024/03.py
@@ -1,5 +1,19 @@
-data = open("03.txt").read().splitlines()
+import re
+
+data = open("03.txt").read().replace("\n", "")
 
 
-print(f"PART ONE: {None}")
-print(f"PART TWO: {None}")
+def sum_valid_mul_instructions(instruction: str) -> int:
+    valid_instructions = re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", instruction)
+    return sum(int(a) * int(b) for a, b in valid_instructions)
+
+
+total_valid_instructions = sum_valid_mul_instructions(instruction=data)
+
+do_instructions = re.findall(r"do\(\)(.*?)don't\(\)", f"do(){data}don't()")
+do_instructions = "".join(do_instructions)
+total_do_instructions = sum_valid_mul_instructions(instruction=do_instructions)
+
+
+print(f"PART ONE: {total_valid_instructions}")
+print(f"PART TWO: {total_do_instructions}")
